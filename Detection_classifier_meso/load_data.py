8eaf2762c8b660984d97a73c135a6dbd308ac737
Isabelle-ensta <isabelle.neveu@ensta-bretagne.org>
2025-03-13 22:01:40 +0100

    Mise à jour classifier Meso

--- Detection_classifier_meso/load_data.py
+++ Detection_classifier_meso/load_data.py
@@ -1,10 +1,18 @@
 from datasets import load_dataset
-from torch.utils.data import Dataset, DataLoader, random_split
+from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 from PIL import Image
 import torch
 import matplotlib.pyplot as plt
 from collections import Counter
+import numpy as np
+from torch.utils.data import Subset
+import random
+from sklearn.model_selection import train_test_split
+
+
+
+#################################################################################################
 
 
 class ImageLabelDataset(Dataset):
@@ -17,7 +25,11 @@ class ImageLabelDataset(Dataset):
 
     def __getitem__(self, idx):
         # Récupérer l'image qui est déjà un PngImageFile
-        image = self.dataset['train'][idx]['images']
+        image = self.dataset['train'][idx]['image']
+
+            # Convertir RGBA → RVB si nécessaire
+        if image.mode == 'RGBA':
+            image = image.convert('RGB')
         
         # Pas besoin de réouvrir l'image si elle est déjà au format PngImageFile
         if isinstance(image, Image.Image):
@@ -30,12 +42,13 @@ class ImageLabelDataset(Dataset):
         
         # Appliquer les transformations sur l'image
         if self.transform:
+            if image.mode != "RGB":
+                image = image.convert("RGB")
             img = self.transform(img)
-
         return img, label
     
     def afficher(self,idx):
-        image = self.dataset['train'][idx]['images']
+        image = self.dataset['train'][idx]['image']
         
         # Pas besoin de réouvrir l'image si elle est déjà au format PngImageFile
         if isinstance(image, Image.Image):
@@ -58,14 +71,13 @@ class ImageLabelDataset(Dataset):
         plt.title(f"Label {label}")
         plt.axis('off')  # Ne pas afficher les axes
         plt.show()
-def count_classes(subset):
+    def count_classes(subset):
         """Compte le nombre d'échantillons par classe dans un torch.utils.data.dataset.Subset."""
     
-        # Récupérer le dataset d'origine et les indices du subset
-        full_dataset = subset.dataset  # Le dataset complet
+        full_dataset = subset.dataset
 
         indices = subset.indices
-    # Indices des échantillons dans le subset
+        # Indices des échantillons dans le subset
 
         # Récupérer les labels des échantillons du subset
         labels = [full_dataset.dataset['train'][i]['fake'] for i in indices]  # Supposons que le label est à l'index 1
@@ -74,64 +86,116 @@ def count_classes(subset):
         class_counts = Counter(labels)
     
         return class_counts
+    
 
+#######################################################################################################
 
 
 if __name__ == "__main__":
 
-    dataset = load_dataset('florian-morel22/deepfake-diff-gan-vqvae')
-    # Charger un dataset localement depuis un chemin spécifique
-    # Afficher un aperçu
-    print(dataset)
+    dataset = load_dataset("florian-morel22/deepfake-celeba")
+    #print(dataset)
+    # Transformation des images et normalisation
 
-    mean=[0.5246, 0.4264, 0.3750]
-    std=[0.2590, 0.2357, 0.2308]
+    mean = [0.5042836 , 0.4235159 , 0.38155344]
+    std = [0.30637124 ,0.28822005, 0.28821015]
 
 
-    # Transformation des images
     transform = transforms.Compose([
-        transforms.Resize((256, 256)),  # Redimensionner les images à une taille uniforme
+        transforms.Resize((256, 256)),  
         transforms.ToTensor(),
-        transforms.Normalize(mean=mean, std=std) ])  # Convertir les images en tenseur
+        transforms.Normalize(mean=mean, std=std) ])  
 
-    # Créer le Dataset personnalisé
+    # Dataset personnalisé
     train_dataset = ImageLabelDataset(dataset, transform=transform)
-    # Calculer la taille du dataset pour chaque partition
-    train_size = int(0.7 * len(train_dataset))  
-    val_size = int(0.15 * len(train_dataset))
-    test_size=len(train_dataset) - train_size - val_size
-    # Diviser le dataset en train et validation
-    train_subset, val_subset, test_subset = random_split(train_dataset, [train_size, val_size, test_size])
+
+    #######################################################################################
+    #reface_subset (1911 images réelles et 1911 deepfake with REFace )
+
+    real_indices = [i for i in range(len(dataset['train'])) if dataset['train'][i]['model'] == 'Real']
+    real_indices = random.sample(real_indices, 1911)  # Sélectionner un sous-ensemble de réels
+    fake_indices = [i for i in range(len(dataset['train'])) if dataset['train'][i]['model'] == 'REFace']
+
+    # Diviser chaque classe en train / val / test avec train_test_split
+    train_real, temp_real = train_test_split(real_indices, test_size=0.2, random_state=42)
+    val_real, test_real = train_test_split(temp_real, test_size=0.5, random_state=42)
+    train_fake, temp_fake = train_test_split(fake_indices, test_size=0.2, random_state=42)
+    val_fake, test_fake = train_test_split(temp_fake, test_size=0.5, random_state=42)
+
+    # Fusionner les indices pour former les datasets finaux
+    train_indices = train_real + train_fake
+    val_indices = val_real + val_fake
+    test_indices = test_real + test_fake
+
+    # Créer les Subsets
+    train_reface_subset = Subset(train_dataset, train_indices)
+    val_reface_subset = Subset(train_dataset, val_indices)
+    test_reface_subset = Subset(train_dataset, test_indices)
+
+    print(f"Taille du dataset d'entraînement (ReFace) : {len(train_reface_subset)}")
+    print(f"Taille du dataset de validation (ReFace) : {len(val_reface_subset)}")
+    print(f"Taille du dataset de test (ReFace) : {len(test_reface_subset)}")
+
+    # Vérification des distributions
+    train_counts = ImageLabelDataset.count_classes(train_reface_subset)
+    val_counts = ImageLabelDataset.count_classes(val_reface_subset)
+    test_counts = ImageLabelDataset.count_classes(test_reface_subset)
+
+    print("Distribution dans le train (ReFace) :", train_counts)
+    print("Distribution dans la validation (ReFace) :", val_counts)
+    print("Distribution dans le test (ReFace) :", test_counts)
 
     torch.save({
-        'train_dataset': train_subset,
-        'val_dataset': val_subset,
-        'test_dataset': test_subset
-    }, "datasets.pth")
+        'train': train_reface_subset,
+        'val': val_reface_subset,
+        'test': test_reface_subset
+    }, "reface_dataset.pth")
 
-    print("Datasets sauvegardés avec succès dans 'datasets.pth' !")
+    print("Datasets sauvegardés avec succès dans 'reface_dataset.pth' !")
 
+    #######################################################################################
+    #e4s_subset (2000 images réelles et 2000 deepfake with e4s )
+    real_indices = [i for i in range(len(dataset['train'])) if dataset['train'][i]['model'] == 'Real']
+    real_indices = real_indices_subset = random.sample(real_indices, 2000)
+    fake_indices = [i for i in range(len(dataset['train'])) if dataset['train'][i]['model'] == 'e4s' ]
 
-    ### Exploration 
+    train_real, temp_real = train_test_split(real_indices, test_size=0.2, random_state=42)
+    val_real, test_real = train_test_split(temp_real, test_size=0.5, random_state=42)
+    train_fake, temp_fake = train_test_split(fake_indices, test_size=0.2, random_state=42)
+    val_fake, test_fake = train_test_split(temp_fake, test_size=0.5, random_state=42)
 
-    print('Le nombre d images entrainées est',len(train_subset))
-    print('Le nombre d images pour la validation est',len(val_subset))
-    print('Le nombre d images pour les test est',len(test_subset))
+    train_indices = train_real + train_fake
+    val_indices = val_real + val_fake
+    test_indices = test_real + test_fake
+
+    train_e4s_subset = Subset(train_dataset, train_indices)
+    val_e4s_subset = Subset(train_dataset, val_indices)
+    test_e4s_subset = Subset(train_dataset, test_indices)
+
+    print(f"Taille du dataset d'entraînement : {len(train_e4s_subset)}")
+    print(f"Taille du dataset de validation : {len(val_e4s_subset)}")
+    print(f"Taille du dataset de test : {len(test_e4s_subset)}")
+
+    train_counts = ImageLabelDataset.count_classes(train_e4s_subset)
+    val_counts = ImageLabelDataset.count_classes(val_e4s_subset)
+    test_counts = ImageLabelDataset.count_classes(test_e4s_subset)
+
+    print("Distribution dans le train (E4S) :", train_counts)
+    print("Distribution dans la validation (E4S) :", val_counts)
+    print("Distribution dans le test (E4S) :", test_counts)
+
+    torch.save({
+        'train': train_e4s_subset,
+        'val': val_e4s_subset,
+        'test': test_e4s_subset
+    }, "e4s_dataset.pth")
+
+    print("Datasets sauvegardés avec succès dans 'e4s_dataset.pth' !")
+    
 
 
     
-    # Exemple d'utilisation sur les subsets d'entraînement, validation et test
-    train_counts = count_classes(train_subset)
-    val_counts = count_classes(val_subset)
-    test_counts = count_classes(test_subset)
 
-    print("Distribution dans le train :", train_counts)
-    print("Distribution dans la validation :", val_counts)
-    print("Distribution dans le test :", test_counts)
 
-#Il y a deux fois plus de 0 que de 1 
 
-#Distribution dans le train : Counter({0: 169, 1: 76}) 245
-#Distribution dans la validation : Counter({0: 36, 1: 16}) 52
-#Distribution dans le test : Counter({0: 29, 1: 25}) 54
 
